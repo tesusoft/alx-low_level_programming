350746ec7cd5e7fcb7cfa266f0fc0802ae4744e1
tesusoft <tesukidney@gmail.com>
2023-03-18 12:08:36 -0700

    Last ccheck for task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,22 +4,28 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
+/**
+ * int main - run my code
+ * @my_arg: n
+ * Description: This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the
+ * number stored in the variable n is positive or negative.
+ * */
+int main(void) 
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if ( n == 0)
+	if (n==0)
 	{
 		printf("%d is zero\n", n);
 	}
-	if ( n < 0)
+	if (n<0)
 	{
 		printf("%d is negative\n", n);
 	}
-	if ( n > 0)
+	if (n>0)
 	{
 		printf("%d is positive\n", n);
 	}
