e33fa73c4fd028119de57100d0a20fccb83d5146
tesusoft <tesukidney@gmail.com>
2023-03-22 07:49:37 -0700

    Pushed a clean 1-alphabet, 1-main.c code file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/*
+* File: 1-alphabet.c
+* Auth: Oyetesu Olumide
+*/
+
+#include "main.h"
+
+/**
+* print_alphabet - prints the alphabet in lowercase, followed by an new line.
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* main - calls the print_alphabet(void);
+*
+* Return: always 0.
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
