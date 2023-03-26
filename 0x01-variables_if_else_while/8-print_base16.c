64687c9bae54e0f4ae0ca6b88fdb9f2115a97062
tesusoft <tesukidney@gmail.com>
2023-03-26 01:36:29 -0700

    Uploaded 8-print_base16, clean neat code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+/*
+*File: 8-print_base16.c
+*Aut: Oyetesu Olumide
+*/
+
+#include <stdio.h>
+
+/**
+* main - Prints all numbers of base 16 in lowercase.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
